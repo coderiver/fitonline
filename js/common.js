91740d960e946b90771d6e99bf5d584f7264520a
dpismenny <denis.pismenny@gmail.com>
2013-05-15 16:44:52 +0300

    sync

--- js/common.js
+++ js/common.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 		});
 
 
-	// ---------- DATEPICKER -------------------- //
+	// ---------- Calendar Datepicker -------------------- //
 		$('#simple-calendar').DatePicker({
 		  mode: 'multiple',
 		  inline: true,
